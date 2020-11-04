873c372f96fabd87746181b33a44c5c013fdf80c
Mozart Brito <mozartcomart@gmail.com>
2020-11-03 22:23:05 -0300

    Updates

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php 
+<?php
+//$senha = 'M0jh#ç54562';
+
+/*if (password_verify('12345', $hash)) {
+    echo 'Password is valid!';
+} else {
+    echo 'Invalid password.'. '<br>';
+}*/
+
+/*echo password_hash('$2y$10$dOCWkCgKERjt1h.UxioQ2.LCkmTwdTyS4vPGPtpWk3wH4M0XUE9am', PASSWORD_BCRYPT); exit;*/
+
   include_once('layout/header.php');
   include_once('layout/menu.php'); 
   include_once('layout/sidebar.php'); 
